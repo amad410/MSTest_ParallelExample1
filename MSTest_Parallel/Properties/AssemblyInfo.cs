516c49c0c60b402cb438a03c190bdbc5c3f4f1a0
Antwan Maddox <antwan.maddox@agilethought.com>
2019-12-13 13:55:30 -0500

    initial commit

---
+++ MSTest_Parallel/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MSTest_Parallel")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MSTest_Parallel")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("14539bb2-a253-48e4-bcdb-7dfb6302df70")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MSTest_Parallel/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+[assembly: Parallelize(Workers =2, Scope =ExecutionScope.MethodLevel)]
+namespace MSTest_Parallel
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IWebDriver driver;
+        [TestMethod]
+        public void TestMethod1()
+        {
+            driver.Navigate().GoToUrl("http://www.espn.com");
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            driver.Navigate().GoToUrl("http://www.espn.com");
+        }
+
+        [TestInitialize]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            driver.Close();
+        }
+    }
+}
